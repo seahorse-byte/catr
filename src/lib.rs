202086ca61849cc9b1b7f801cca4569909b812e0
Olsi Gjeci <olsi.developer@gmail.com>
2024-02-20 20:04:40 +0000

    read file and print lines

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
-use std::error::Error;
+use std::{
+    error::Error,
+    fs::File,
+    io::{self, BufRead, BufReader},
+};
 
 use clap::{App, Arg};
 
@@ -29,25 +33,50 @@ pub fn get_args() -> MyResult<Config> {
             Arg::with_name("number_lines")
                 .short("n")
                 .long("number")
-                .help("Number all output lines"),
+                .help("Number all output lines")
+                .takes_value(false)
+                .conflicts_with("number_nonblank"),
         )
         .arg(
-            Arg::with_name("number_nonblank_lines")
+            Arg::with_name("number_nonblank")
                 .short("b")
                 .long("number-nonblank")
-                .help("Number nonempty output lines"),
+                .help("Number nonempty output lines")
+                .takes_value(false),
         )
         .get_matches();
 
-        Ok(Config {
-            files: matches.values_of_lossy("files").unwrap(),
-            number_lines: matches.is_present("number_lines"),
-            number_nonblank_lines: matches.is_present("number_nonblank_lines")
-        })
+    Ok(Config {
+        files: matches.values_of_lossy("files").unwrap(),
+        number_lines: matches.is_present("number_lines"),
+        number_nonblank_lines: matches.is_present("number_nonblank_lines"),
+    })
+}
 
+pub fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
 }
 
 pub fn run(config: Config) -> MyResult<()> {
-    dbg!(config);
+    for filename in config.files {
+        match open(&filename) {
+            Err(e) => eprintln!("Failed to open {}: {}", filename, e),
+            Ok(file) => {
+                for line_result in file.lines() {
+                    let mut line_num = 0;
+                    let line = line_result?;
+                    line_num += 1;
+                    if config.number_lines {
+                        println!("{:6} {}", line_num, line);
+                    } else {
+                        println!("{}", line);
+                    }
+                }
+            }
+        }
+    }
     Ok(())
 }
